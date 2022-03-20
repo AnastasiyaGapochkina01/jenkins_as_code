885733f4b28bbf92f41c84f2c54051d4f8a58981
Anastasiya <agapochkina@tradingview.com>
2022-03-20 15:55:01 +0300

    Add docker folder with all files needs to build

---
+++ docker/import_jobs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+JENKINS_URL="http://localhost:8080"
+JENKINS_ADMIN_USER="agapochkina"
+JENKINS_ADMIN_PASS="admin123"
+LIST_OF_JOBS=$(ls -1 /opt/jobs/ | sed 's/\..*//')
+
+for job in ${LIST_OF_JOBS}
+do
+  java -jar /var/jenkins_home/jenkins-cli.jar -s ${JENKINS_URL} -auth ${JENKINS_ADMIN_USER}:${JENKINS_ADMIN_PASS} create-job ${job} < /opt/jobs/${job}.xml
+done
